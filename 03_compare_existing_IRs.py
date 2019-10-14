5f8a278a15e27647d43f079e3f4e60d312cc7c5f
Tilman Mehl <tilmanmehl@ZEDAT.FU-Berlin.DE>
2019-10-14 16:03:37 +0200

    IR comparisons are written to output file now

--- 03_compare_existing_IRs.py
+++ 03_compare_existing_IRs.py
@@ -63,7 +63,7 @@ __author__ = 'Michael Gruenstaeudl <m.gruenstaeudl@fu-berlin.de>, '\
              'Tilman Mehl <tilmanmehl@zedat.fu-berlin.de>'
 __copyright__ = 'Copyright (C) 2019 Michael Gruenstaeudl and Tilman Mehl'
 __info__ = 'Compare IRs for a series of IR FASTA files'
-__version__ = '2019.10.07.1400'
+__version__ = '2019.10.14.1600'
 
 #############
 # DEBUGGING #
@@ -84,8 +84,24 @@ def main(args):
   # STEP 2. Loop though provided archives
     archives = [os.path.abspath(x) for x in args.input]
     main_dir = os.getcwd()
+    # Check if outfile exists, write headers
+    # Raise an error if outfile exists but does not begin with the required headers
+    if not os.path.isfile(args.outfn):
+        with open(args.outfn, "w") as outfile:
+            outfile.write("ACCESSION\tIR_COUNT\tLEN_A\tLEN_B\tLEN_DIFF\tSNP_COUNT\n")
+    else:
+        with open(args.outfn, "r") as outfile:
+            if not outfile.readline() == "ACCESSION\tIR_COUNT\tLEN_A\tLEN_B\tLEN_DIFF\tSNP_COUNT\n":
+                raise Exception('Malformed output file!')
+
     for archive in archives:
+        # Init values that will be written to table
         accession = os.path.basename(archive).split('.')[0]
+        ir_count = 0
+        len_a = None
+        len_b = None
+        len_diff = None
+        snp_count = None
         # Extract sequence file from archive
         tar = tarfile.open(archive, "r:gz")
         tar.extractall()
@@ -96,6 +112,13 @@ def main(args):
         # Check if two IRs exist for this accession
         if os.path.isfile(accession + "_IRa.fasta") and os.path.isfile(accession + "_IRb_revComp.fasta"):
             log.info("Found both IRs for accession " + accession)
+            ir_count = 2
+            with open(accession + "_IRa.fasta", "r") as ira:
+                len_a = len(ira.readlines()[1])
+            with open(accession + "_IRb_revComp.fasta", "r") as irb:
+                len_b = len(irb.readlines()[1])
+            len_diff = abs(len_a - len_b)
+
             # Process IRs with mummer
             log.info("Comparing IR FASTAs using mummer's nucmer function.")
             nucmerargs = ["nucmer", "--maxmatch", "-c", "100", "-p", accession, accession + "_IRa.fasta", accession + "_IRb_revComp.fasta"]
@@ -142,7 +165,7 @@ def main(args):
                       if(len(line) > 0):
                           if line[0] == '>':
                               if "IRa" in line:
-                                  outfile.write(line + "\n")                                  
+                                  outfile.write(line + "\n")
                               else:
                                   outfile.write("\n" + line + "\n")
                           else:
@@ -153,24 +176,27 @@ def main(args):
 
             # Numerical comparison via command 'cmp'
             log.info("Comparing differences between IR sequences using generated alignment.")
-            ira_aln = subprocess.Popen(["sed","-n","2p",accession + "_clustalo_deint.fasta"], stdout=subprocess.PIPE)
-            #ira_aln.wait()
-            ira_out, ira_err = ira_aln.communicate()
-            irb_aln = subprocess.Popen(["sed","-n","4p",accession + "_clustalo_deint.fasta"], stdout=subprocess.PIPE)
-            #irb_aln.wait()
-            irb_out, irb_err = irb_aln.communicate()
             with open(accession + ".compare","w") as comparefile:
                 cmp_awk = subprocess.Popen("cmp -bl <(sed -n '2p' " + accession + "_clustalo_deint.fasta) <(sed -n '2p' " + accession + "_clustalo_deint.fasta) | awk '{print $1,$3,$5}'", shell=True, executable="/bin/bash", stdout=comparefile)
                 cmp_awk.wait()
-
+            with open(accession + ".compare","r") as comparefile:
+                snp_count = len(comparefile.readlines())
             # Append created files to archive
             log.info("Archiving generated files.")
             tar = tarfile.open(archive, "w:gz")
             tar.add(acc_path, accession)
             tar.close()
+        elif os.path.isfile(accession + "_IRa.fasta") ^ os.path.isfile(accession + "_IRb_revComp.fasta"):
+            log.info("Missing one IR for accession " + accession + "! Skipping comparison.")
+            ir_count = 1
         else:
-            log.info("Missing IR(s) for accession " + accession + "! Skipping comparison.")
+            log.info("Missing both IRs for accession " + accession + "! Skipping comparison.")
+
+        log.info("Writing gathered information to output.")
         os.chdir(main_dir)
+        with open(args.outfn,"a") as outfile:
+            outfile.write("%s\t%s\t%s\t%s\t%s\t%s\n" % (str(accession), str(ir_count), str(len_a), str(len_b), str(len_diff), str(snp_count)))
+
 
 
 ########
@@ -179,7 +205,7 @@ def main(args):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="  --  ".join([__author__, __copyright__, __info__, __version__]))
-    parser.add_argument("--input", "-i", type=str, required=True, nargs='+', help="List of archive file names containing inverted repeat FASTA files")
-    #parser.add_argument("-outfn", "-o", type=str, required=True, help="Name of the file the results of the IR testing will be written to")
+    parser.add_argument("--input", "-i", type=str, required=True, nargs='+', help="List of archive file paths containing inverted repeat FASTA files")
+    parser.add_argument("-outfn", "-o", type=str, required=True, help="Name of the file the results of the IR testing will be written to")
     args = parser.parse_args()
     main(args)
