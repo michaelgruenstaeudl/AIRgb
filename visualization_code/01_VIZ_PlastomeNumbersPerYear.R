ff7d2acd23be0f51b0c9dc0dc06edeb9c9aad9a2
Michael Gruenstaeudl <mi.gruenstaeudl@gmail.com>
2019-11-19 15:51:03 +0100

    Updated visualizations

--- visualization_code/01_VIZ_PlastomeNumbersPerYear.R
+++ visualization_code/01_VIZ_PlastomeNumbersPerYear.R
@@ -33,7 +33,7 @@ plotData = data.frame(DATE=as.Date(names(tab)), FREQUENCY=as.vector(tab))
 ########################################################################
 
 base_plot = ggplot(data=plotData, aes(x=DATE, y=cumsum(plotData[,"FREQUENCY"])), width=1) + 
-    geom_bar(stat="identity", position="identity", color="grey", fill="grey", alpha=0.5) #+ 
+    geom_bar(stat="identity", position="identity", fill="grey50", alpha=0.5) #+ 
     #geom_line(color="grey", alpha=0.5)
 
 myPlot = base_plot + 
@@ -45,17 +45,26 @@ myPlot = base_plot +
                  date_breaks="1 year", minor_breaks=NULL, expand=expand_scale(0),
                  date_labels="%Y") + 
     scale_y_continuous(breaks=seq(0, 6000, 1000), minor_breaks=seq(500, 5500, 1000)) +
+    #scale_colour_grey(aesthetics = "fill") + 
+    #scale_fill_brewer(palette="Dark2", name="Criterion positive/negative") + 
+    scale_fill_manual(values=c("grey50", "grey0"), name="Plastid genome\navailable", labels=c("Yes", "No")) +
     #theme_bw() + 
     theme_minimal() + 
     theme(plot.title = element_text(size=20),
           plot.subtitle = element_text(size=16, face="italic"),
           axis.text=element_text(size=14),
           axis.title=element_text(size=16, face="bold"),
-          plot.margin=unit(c(0.5,1.0,0.1,0.1),"cm"))  # Note: margin(t=0, r=0, b=0, l=0, unit="pt")
+          plot.margin=unit(c(0.5,1.0,0.1,0.1),"cm"),  # Note: margin(t=0, r=0, b=0, l=0, unit="pt")
+          legend.key.width=unit(1,"cm"))
 
 ########################################################################
 
-svglite(file=paste(out_fn, "VIZ_PlastomeNumbersPerYear.svg", sep=''), width=21, height=14.85)
+PlastomeNumbersPerYear = myPlot
+save(PlastomeNumbersPerYear, file="VIZ_PlastomeNumbersPerYear.Rdata")
+
+########################################################################
+
+svglite(file=paste(out_fn, "VIZ_PlastomeNumbersPerYear.svg", sep=''), width=21, height=7.425)
 myPlot
 dev.off()
 
--- visualization_code/02a_VIZ_PlastomesWithoutReportedIRsPerYear.R
+++ visualization_code/02a_VIZ_PlastomesWithoutReportedIRsPerYear.R
@@ -3,7 +3,7 @@
 #copyright = "Copyright (C) 2019 Michael Gruenstaeudl"
 #contributors = c("Michael Gruenstaeudl")
 #email = "m.gruenstaeudl@fu-berlin.de"
-#version = "2019.11.15.2000"
+#version = "2019.11.19.1130"
 
 ########################################################################
 
@@ -11,6 +11,7 @@ library(ggplot2)
 library(svglite)
 library(tcltk) # For dialog boxes
 library(tools) # For function 'file_path_sans_ext'
+library(dplyr) # For function '%>%'
 
 ########################################################################
 
@@ -44,30 +45,37 @@ posPlotData = data.frame(DATE=as.Date(names(posTab)), FREQ_RECORDS=as.vector(pos
 negPlotData = data.frame(DATE=as.Date(names(negTab)), FREQ_RECORDS=as.vector(negTab), CRITERION="negative")
 ## Concatenate plot data parts
 plotData = rbind(posPlotData, negPlotData)
+## Resort plot data
+plotData = plotData[order(plotData$DATE),]
+## Add a column that displays the growing cumulative sum for each criterion
+plotData = plotData %>% group_by(CRITERION) %>% mutate(CUMFREQ=cumsum(FREQ_RECORDS))
 
 ########################################################################
 
-base_plot = ggplot(data=plotData, aes(x=DATE, y=cumsum(plotData[,"FREQ_RECORDS"])), width=1, fill=CRITERION) + 
-    geom_bar(stat="identity", position="dodge", alpha=0.5) ##side-by-side barcharts
-    #geom_bar(stat="identity", position="identity", alpha=0.25) + ##overlaid barcharts
-    #geom_bar(stat="identity", alpha=0.5) + ##stacked barcharts
+base_plot = ggplot(data=plotData, aes(x=DATE, y=CUMFREQ, fill=CRITERION), width=1) + 
+    geom_bar(stat="identity", position="stack", alpha=0.5) # stacked barcharts
+    #geom_bar(stat="identity", position="dodge", alpha=0.5) # side-by-side barcharts
 
 myPlot = base_plot + 
     xlab("\nYear") + 
     ylab("Total Number of Records\n") + 
-    ggtitle("Number of complete plastid genome sequences available on NCBI GenBank per submission year, \nseparated by presence of annotation for the IR",
+    ggtitle("Number of complete plastid genome sequences available on NCBI GenBank per submission year, \nseparated by presence of IR annotation",
             subtitle="Note: Only plastid genomes of angiosperms are counted") + 
     scale_x_date(limits=c(as.Date("2000-01-01"), as.Date("2020-01-01")),
                  date_breaks="1 year", minor_breaks=NULL, expand=expand_scale(0),
                  date_labels="%Y") + 
+    scale_y_continuous(breaks=seq(0, 6000, 1000), minor_breaks=seq(500, 5500, 1000)) +
+    #scale_colour_grey(aesthetics = "fill") + 
     #scale_fill_brewer(palette="Dark2", name="Criterion positive/negative") + 
+    scale_fill_manual(values=c("grey50", "grey0"), name="IR annotation\npresent", labels=c("Yes", "No")) + 
     #theme_bw() + 
     theme_minimal() + 
     theme(plot.title = element_text(size=20),
           plot.subtitle = element_text(size=16, face="italic"),
           axis.text=element_text(size=14),
           axis.title=element_text(size=16, face="bold"),
-          plot.margin=unit(c(0.5,1.0,0.1,0.1),"cm"))  # Note: margin(t=0, r=0, b=0, l=0, unit="pt")
+          plot.margin=unit(c(0.5,1.0,0.1,0.1),"cm"),  # Note: margin(t=0, r=0, b=0, l=0, unit="pt")
+          legend.key.width=unit(1,"cm"))
 
 ################################
 
@@ -88,10 +96,14 @@ myPlot = base_plot +
 #        #ggsubtitle("Only submissions to the plant database have been counted.\nNote: Y-axis has been square-root transformed for better visibility")
 #    myPlot_transformed = myPlot_transformed + annotation_logticks(base=10, sides="l")
 
+########################################################################
+
+PlastomesWithWithoutIRAnnos = myPlot
+save(PlastomesWithWithoutIRAnnos, file="VIZ_PlastomesWithWithoutIRAnnos.Rdata")
 
 ########################################################################
 
-svglite(file=paste(out_fn, "VIZ_PlastomesWithWithoutIRAnnos.svg", sep=''), width=17, height=11.5)
+svglite(file=paste(out_fn, "VIZ_PlastomesWithWithoutIRAnnos.svg", sep=''), width=21, height=7.425)
 myPlot
 dev.off()
 
